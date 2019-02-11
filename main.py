4e82ff9f6aa50dfb81b812d7b98e803af9ed0974
Spencer Shadley <spencershadley@utexas.edu>
2019-02-10 20:47:13 -0800

    updated strings

--- main.py
+++ main.py
@@ -8,22 +8,22 @@ target = input("Enter target file with full path: ")
 try:
     copyfile(source, target)
 except IOError as e:
-    print("Unable to copy file. %s" % e)
+    print("Unable to copy file. Error: " + e)
     exit(1)
 except:
-    print("Unexpected error:", sys.exc_info())
+    print("Unexpected error: ", sys.exc_info())
     exit(1)
 
 print("\nFile copy done!\n")
 
 while True:
-    print("Do you like to print the file ? (y/n): ")
+    print("Do you want to print the file ? (y/n): ")
     check = input()
     if check == 'n':
         break
     elif check == 'y':
         file = open(target, "r")
-        print("\nHere follows the file content:\n")
+        print("\nFile contents:\n")
         print(file.read())
         file.close()
         print()
