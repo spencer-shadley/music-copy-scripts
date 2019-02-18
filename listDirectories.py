391f49e02145efd98bfae01b33ac3e053b87c547
Spencer Shadley <spencershadley@utexas.edu>
2019-02-17 17:45:57 -0800

    fixed deletion logic, added empty directory insertion logic

--- listDirectories.py
+++ listDirectories.py
@@ -1,37 +1,55 @@
-from os import walk
-from shutil import rmtree
+from os import walk, path, makedirs, listdir
+from shutil import rmtree, copyfile
 
+# retrieve settings
 settingsFile = open('./settings.txt', 'r')
 directoryToReadFrom = settingsFile.readline().strip()
+directoryPrefix = '_'
 directorySuffix = settingsFile.readline().strip()
 likedMusic = settingsFile.readline().strip()
 categories = settingsFile.readline().strip().split(',')
 
+# store files found
+musicTypes = listdir(directoryToReadFrom)
 directoriesToParse = [directoryToReadFrom]
 filesFound = []
 
-# delete any existing copied directories
-for(currPath, directoryNames, fileNames) in walk(directoryToReadFrom):
-    for directoryName in directoryNames:
-        if directorySuffix in directoryName:
-            directoryToDelete = currPath + '\\' + directoryName
-            rmtree(directoryToDelete)
-            print('deleted: ' + directoryToDelete)
+for musicType in musicTypes:
+    musicTypePath = path.join(directoryToReadFrom, musicType)
+
+    # delete existing flattened directories
+    for musicSeries in listdir(musicTypePath):
+        if directorySuffix in musicSeries:
+            seriesToDelete = path.join(musicTypePath, musicSeries)
+            rmtree(seriesToDelete)
+            print('deleted: ' + seriesToDelete)
+
+    # create empty directories to copy into
+    for category in categories:
+        flattenedDirectory = musicTypePath + '\\' + directoryPrefix + category + directorySuffix
+        if not path.exists(flattenedDirectory):
+            makedirs(flattenedDirectory)
+
+# create (empty) flattened directories per child of directory
+# for(currPath, directoryNames, fileNames) in walk(directoryToReadFrom):
+#     for category in categories:
+#         makedirs(directoryToReadFrom + '\\' + category + directorySuffix)
 
 # find files to copy
 for directory in directoriesToParse:
     for(currPath, directoryNames, fileNames) in walk(directory):
-        for directoryName in directoryNames:
-            directoriesToParse.append(currPath + directoryName)
+        for musicType in directoryNames:
+            directoriesToParse.append(currPath + musicType)
+
         for fileName in fileNames:
             filePath = currPath + '\\' + fileName
             if likedMusic in currPath and directorySuffix not in currPath:
+                copyfile(filePath, directoryToReadFrom + '\\' + likedMusic + directorySuffix + '\\' + fileName)
                 print('liked music: ' + filePath)
             filesFound.append(filePath)
     directoriesToParse.remove(directory)
 
 print(filesFound)
-
 
 # 1. delete existing suffixed directories
 # 2. create suffixed directories
