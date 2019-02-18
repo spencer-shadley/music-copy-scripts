0352e576871318f905621dc14624afae6d32e068
Spencer Shadley <spencershadley@utexas.edu>
2019-02-17 20:12:32 -0800

    added safety directory check

--- copyToFlattenedFilees.py
+++ copyToFlattenedFilees.py
@@ -36,19 +36,21 @@ for musicType in musicTypes:
 for musicType in listdir(directoryToReadFrom):
     musicTypePath = path.join(directoryToReadFrom, musicType)
     flattenedMusicDirectory = path.join(musicTypePath, directoryPrefix + likedMusic + directorySuffix)
-    for musicSeries in listdir(musicTypePath):
-        musicSeriesPath = path.join(musicTypePath, musicSeries)
-        for musicSeriesDirectory in listdir(musicSeriesPath):
-            musicSeriesDirectoryPath = path.join(musicSeriesPath, musicSeriesDirectory)
-            if path.isdir(musicSeriesDirectoryPath) and likedMusic in musicSeriesDirectoryPath:
-                for likedFileOrGenreDir in listdir(musicSeriesDirectoryPath):
-                    likedFileOrGenreDirPath = path.join(musicSeriesDirectoryPath, likedFileOrGenreDir)
-                    if path.isdir(likedFileOrGenreDirPath):
-                        for genreFile in listdir(likedFileOrGenreDirPath):
-                            genreFilePath = path.join(likedFileOrGenreDirPath, genreFile)
-                            genreFlattenedPath = path.join(musicTypePath, directoryPrefix + likedFileOrGenreDir + directorySuffix)
-                            copyfile(genreFilePath, path.join(genreFlattenedPath, genreFile))
-                    else:
-                        likedFileDestPath = path.join(flattenedMusicDirectory, likedFileOrGenreDir)
-                        copyfile(likedFileOrGenreDirPath, likedFileDestPath)
-                print(musicSeriesDirectoryPath)
+
+    if path.isdir(musicTypePath):
+        for musicSeries in listdir(musicTypePath):
+            musicSeriesPath = path.join(musicTypePath, musicSeries)
+            for musicSeriesDirectory in listdir(musicSeriesPath):
+                musicSeriesDirectoryPath = path.join(musicSeriesPath, musicSeriesDirectory)
+                if path.isdir(musicSeriesDirectoryPath) and likedMusic in musicSeriesDirectoryPath:
+                    for likedFileOrGenreDir in listdir(musicSeriesDirectoryPath):
+                        likedFileOrGenreDirPath = path.join(musicSeriesDirectoryPath, likedFileOrGenreDir)
+                        if path.isdir(likedFileOrGenreDirPath):
+                            for genreFile in listdir(likedFileOrGenreDirPath):
+                                genreFilePath = path.join(likedFileOrGenreDirPath, genreFile)
+                                genreFlattenedPath = path.join(musicTypePath, directoryPrefix + likedFileOrGenreDir + directorySuffix)
+                                copyfile(genreFilePath, path.join(genreFlattenedPath, genreFile))
+                        else:
+                            likedFileDestPath = path.join(flattenedMusicDirectory, likedFileOrGenreDir)
+                            copyfile(likedFileOrGenreDirPath, likedFileDestPath)
+                    print(musicSeriesDirectoryPath)
