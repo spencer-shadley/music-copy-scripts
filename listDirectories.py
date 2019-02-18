ac5682bef807661244b233fafbce43fb5f8460c8
Spencer Shadley <spencershadley@utexas.edu>
2019-02-17 16:38:40 -0800

    added categories and parsing logic

--- listDirectories.py
+++ listDirectories.py
@@ -1,12 +1,22 @@
 from os import walk
 
-f = []
+categoriesFile = open('./categories.txt', 'r')
+directorySuffix = categoriesFile.readline().strip()
+likedMusic = categoriesFile.readline().strip()
+categories = categoriesFile.readline().strip()
+
 directoriesToParse = ['H:\\']
+filesFound = []
 
 for directory in directoriesToParse:
     for(currPath, directoryNames, fileNames) in walk(directory):
         for directoryName in directoryNames:
             directoriesToParse.append(currPath + directoryName)
         for fileName in fileNames:
-            print(currPath + fileName)
+            filePath = currPath + '\\' + fileName
+            if likedMusic in currPath and directorySuffix not in currPath:
+                print('liked music: ' + filePath)
+            filesFound.append(filePath)
     directoriesToParse.remove(directory)
+
+print(filesFound)
