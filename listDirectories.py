516eff50a13f2b9e26dbf43702d2cd2c41d8825a
Spencer Shadley <spencershadley@utexas.edu>
2019-02-17 18:36:30 -0800

    added file copying logic - _Like - flattened is done!

--- listDirectories.py
+++ listDirectories.py
@@ -25,6 +25,7 @@ for musicType in musicTypes:
             print('deleted: ' + seriesToDelete)
 
     # create empty directories to copy into
+    makedirs(path.join(musicTypePath, directoryPrefix + likedMusic + directorySuffix))
     for category in categories:
         flattenedDirectory = musicTypePath + '\\' + directoryPrefix + category + directorySuffix
         if not path.exists(flattenedDirectory):
@@ -33,43 +34,16 @@ for musicType in musicTypes:
 # find files to copy
 for musicType in listdir(directoryToReadFrom):
     musicTypePath = path.join(directoryToReadFrom, musicType)
+    flattenedMusicDirectory = path.join(musicTypePath, directoryPrefix + likedMusic + directorySuffix)
     for musicSeries in listdir(musicTypePath):
-        # for (currPath, directoryNames, fileNames) in walk(musicTypePath):
         musicSeriesPath = path.join(musicTypePath, musicSeries)
         for musicSeriesDirectory in listdir(musicSeriesPath):
             musicSeriesDirectoryPath = path.join(musicSeriesPath, musicSeriesDirectory)
             if path.isdir(musicSeriesDirectoryPath) and likedMusic in musicSeriesDirectoryPath:
-                # pull out all files into the Liked flattened dir
                 for likedFile in listdir(musicSeriesDirectoryPath):
                     likedFilePath = path.join(musicSeriesDirectoryPath, likedFile)
                     if not path.isdir(likedFilePath):
-                        print('liked file: ' + likedFilePath)
-                # look for more folders and pull those into their own categories
+                        likedFileDestPath = path.join(flattenedMusicDirectory, likedFile)
+                        copyfile(likedFilePath, likedFileDestPath)
+                # TODO: look for more folders and pull those into their own categories
                 print(musicSeriesDirectoryPath)
-            # for fileName in fileNames:
-            #     filePath = path.join(currPath, fileName)
-            #     if likedMusic in currPath and directorySuffix not in currPath:
-            #         print('found liked music: ' + filePath)
-            #         dest = musicTypePath + ''
-            #         copyfile(filePath, dest)
-
-
-# for directory in directoriesToParse:
-#     for(currPath, directoryNames, fileNames) in walk(directory):
-#         for musicType in directoryNames:
-#             directoriesToParse.append(currPath + musicType)
-#
-#         for fileName in fileNames:
-#             filePath = currPath + '\\' + fileName
-#             if likedMusic in currPath and directorySuffix not in currPath:
-#                 copyfile(filePath, directoryToReadFrom + '\\' + likedMusic + directorySuffix + '\\' + fileName)
-#                 print('liked music: ' + filePath)
-#             filesFound.append(filePath)
-#     directoriesToParse.remove(directory)
-
-print(filesFound)
-
-# 1. delete existing suffixed directories
-# 2. create suffixed directories
-# 3. read through all files
-# 4. if a liked file, copy it to the appropriate suffixed directory
